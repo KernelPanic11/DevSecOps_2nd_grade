3de970c37bb6d81cb2b4fe056600ce0ea37e5383
kernelpanic <roheno9489@gmx.fr>
2026-04-02 11:07:08 +0200

    Fix pipeline #10 - JOB 1

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 app.secret_key = "supersecret123"  # FAILLE : clé secrète en dur et faible
-app.secret_key_api = "ghp_SyA1234567890abcdefghijklmnopqrstuvwxyz"  # FAILLE : clé secrète en dur et faible
+#app.secret_key_api = "ghp_SyA1234567890abcdefghijklmnopqrstuvwxyz"  # FAILLE : clé secrète en dur et faible
 
 DATABASE = "hackboard.db"
 
