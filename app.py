78de60fdc0555041a62f423b53cc9c32b2339e6e
kernelpanic <roheno9489@gmx.fr>
2026-04-02 17:32:32 +0200

    Fix pipeline #21 - JOB 4

--- app.py
+++ app.py
@@ -233,4 +233,5 @@ if __name__ == "__main__":
     if not os.path.exists(DATABASE):
         init_db()
     # FAILLE : debug=True en production
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    # app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
